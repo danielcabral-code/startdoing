f991df175496de36716509aeb15a85b1a276a343
rubenferreira7 <40190329@esmad.ipp.pt>
2020-12-23 12:24:44 +0000

    Project creation

---
+++ StartDoing/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
