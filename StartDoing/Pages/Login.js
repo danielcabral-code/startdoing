d819b3fc7a8bb2938cc59dc1716a5b2c4b5f22c0
Daniel Cabral <1danielcabral@gmail.com>
2020-12-28 16:38:22 +0000

    Progress on design implementation of Login Page

--- StartDoing/Pages/Login.js
+++ StartDoing/Pages/Login.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-import {StyleSheet, View, Image, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 const Login = () => {
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 100 : 100;
+
   return (
     <>
-      <View style={styles.background}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.background}
+        keyboardVerticalOffset={keyboardVerticalOffset}>
         <Image
           source={require('../Images/LogoStartDoing.png')}
           style={styles.logo}></Image>
         <Text style={styles.logoName}>StartDoing</Text>
-      </View>
+        <View style={styles.inputView}>
+          <Text style={styles.inputText}>EMAIL</Text>
+          <TextInput style={styles.inputLine} />
+        </View>
+        <View style={styles.inputView}>
+          <Text style={styles.inputText}>PASSWORD</Text>
+          <TextInput style={styles.inputLine} secureTextEntry={true} />
+        </View>
+      </KeyboardAvoidingView>
     </>
   );
 };
@@ -24,13 +44,29 @@ const styles = StyleSheet.create({
   logo: {
     height: 210,
     width: 184,
-    marginTop: -10
+    marginTop: -10,
   },
   logoName: {
     color: '#FF8A3B',
     fontFamily: 'OpenSans-Light',
     fontSize: 36,
-    marginTop: -22
+    marginTop: -22,
+  },
+  inputView: {
+    marginTop: 30,
+  },
+  inputText: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    color: 'white',
+  },
+  inputLine: {
+    width: 300,
+    height: 40,
+    alignSelf: 'center',
+    borderColor: 'white',
+    borderBottomWidth: 2,
+    color: 'white',
   },
 });
 
