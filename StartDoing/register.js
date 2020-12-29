441566f0bfa430fb4826522e77b4cd02ed247e88
Rúben Ferreira <rubenfferreira1905@gmail.com>
2020-12-29 16:20:21 +0000

    Create account with input verification

--- StartDoing/register.js
+++ StartDoing/register.js
@@ -1,19 +1,53 @@
 import 'react-native-gesture-handler';
-import React, {useState} from 'react';
-import { TextInput, Button, StyleSheet, View, Text, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, View, Text, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Swiper from 'react-native-swiper'
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { color } from 'react-native-reanimated';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import Moment from 'moment'
+
+const convert = require('convert-units')
+ 
+
+
 
 function RegisterScreen({ navigation }) {
+
 
   const [email, setEmail] = React.useState('');
   const [name, setName] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [repeatPassword, setRepeatPassword] = React.useState('');
 
+
+  const checkRegisterInputs = () => {
+    //Check for the Name TextInput
+    if (!name.trim()) {
+      alert('Please Enter Name');
+      return;
+    }
+    if (!email.trim()) {
+      alert('Please Enter Email');
+      return;
+    }
+    if (!password.trim()) {
+      alert('Please Enter Password');
+      return;
+    }
+    if (!repeatPassword.trim()) {
+      alert('Please Enter Password');
+      return;
+    }
+    if (password.trim() !== repeatPassword.trim()) {
+      alert('Password must match');
+      return;
+    }
+    else{
+      navigation.navigate('MoreInfo', { name: name, email: email, password: password })
+
+    }
+    
+  };
   return (
 
     <View>
@@ -24,7 +58,7 @@ function RegisterScreen({ navigation }) {
         onChangeText={text => setName(text)}
         value={name}
       />
-
+      
       <Text>EMAIL</Text>
       <TextInput
         style={{ width: 300, height: 40, alignSelf: 'center', borderColor: 'gray', borderBottomWidth: 2, marginBottom: 20 }}
@@ -50,7 +84,7 @@ function RegisterScreen({ navigation }) {
       />
 
       <Pressable
-        onPressIn={() => navigation.navigate('Results', { name: name, email: email, password: password })}
+        onPressIn={checkRegisterInputs}
         style={{ height: 50, width: 300, backgroundColor: "#F27A29", alignSelf: "center" }}>
         <Text style={{ textAlign: "center", marginTop: 12.5 }}>NEXT</Text>
       </Pressable>
@@ -62,110 +96,135 @@ function RegisterScreen({ navigation }) {
   )
 }
 
-function MoreRegisterInfo({ navigation, route }) {
+function MoreInfo({ navigation, route }) {
+
   const [height, setHeight] = useState('');
   const [birth, setBirth] = useState('');
   const [weight, setWeight] = useState('');
-  const [photoUrl, setPhotoUrl] = useState('');
+  const [showDate, setShowDate] = useState('');
 
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'android');
-    setDate(currentDate);
-    console.log(currentDate);
+  const checkMoreInfoInputs = () => {
+    //Check for the Name TextInput
+    
+    if (!height) {
+      alert('Please Enter Height');
+      return;
+    }
+    if (!weight.trim()) {
+      alert('Please Enter Weight');
+      return;
+    }
+    else{
+      createUser();
+
+    }
     
   };
 
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
   };
 
-  const showDatepicker = () => {
-    showMode('date');
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
   };
 
+  const handleConfirm = (date) => {
+    setBirth(date);
+    setShowDate(Moment(date).format("DD/MM/YYYY"))
+    hideDatePicker();
+    console.log(birth);
+
+  };
+
+  function createUser() {
+  
+    console.log(height)
+    try {
+      fetch('https://startdoing.herokuapp.com/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "name": route.params.name,
+          "email": route.params.email,
+          "password": route.params.password,
+          "height": height,
+          "birth": birth,
+          "weight": weight,
+          "photoUrl": "xskk"
+        })
+
+      })
+      console.log("created");
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
 
   return (
 
-    <Swiper style={styles.wrapper} showsButtons={true}>
-      
-      <View style={styles.slide1}>
-        <Text style={styles.text}>INDICATE YOUR DATE OF BIRTH</Text>
-        <View>
-          <Button onPress={showDatepicker} title="Select date" />
-        </View>
+    <View style={styles.slide1}>
+      <Text style={styles.text}>INDICATE YOUR DATE OF BIRTH</Text>
 
-        {show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            mode={mode}
-            is24Hour={true}
-            display="default"
-            onChange={onChange}
-          />
-        )}
-      </View>
 
-      <View style={styles.slide2}>
-        <Text style={styles.text}>INDICATE YOUR WEIGTH</Text>
+      <Pressable
+        onPressIn={showDatePicker}
+        style={{ height: 50, width: 300, backgroundColor: "#F27A29", alignSelf: "center" }}>
+        <Text style={{ textAlign: "center", marginTop: 12.5 }}>Select date</Text>
+      </Pressable>
+
+      <Text style={styles.text}>{showDate}</Text>
+
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+
+      <Text style={styles.text}>INDICATE YOUR HEIGHT</Text>
+      <View style={{ display: "flex", flexDirection: "row" }}>
         <TextInput
-        style={{ width: 200,height:40, color:"white",marginTop:"58%", borderColor: 'gray', borderBottomWidth: 2, marginBottom: 20 }}
-        onChangeText={text => setHeight(text)}
-        value={height}/>
+          style={{ width: 100, height: 40,color: "white", alignSelf: 'center', borderColor: 'gray', borderBottomWidth: 2, marginBottom: 20 }}
+          onChangeText={text => setHeight(convert(text).from('cm').to('m'))}
+          keyboardType="number-pad"
+          value={height}
+        />
+        <Text style={{ color: "white", marginTop: 25, marginLeft: 10 }}>cm</Text>
       </View>
 
-      <View style={styles.slide3}>
-        <Text style={styles.text}>And simple</Text>
-      </View>
-    </Swiper>
+      <Text style={styles.text}>INDICATE YOUR WEIGHT</Text>
+      <View style={{ display: "flex", flexDirection: "row" }}>
 
-    /* <View style={{ flex: 1 }}>
-      <Text>{route.params.name}</Text>
-      <Text>{route.params.email} {route.params.password}</Text>
-    </View> */
+        <TextInput
+          style={{ width: 100, height: 40,color: "white", alignSelf: 'center', borderColor: 'gray', borderBottomWidth: 2, marginBottom: 20 }}
+          onChangeText={text => setWeight(text)}
+          keyboardType="number-pad"
+          value={weight}
+          
+        />
+        <Text style={{ color: "white", marginTop: 25, marginLeft: 10 }}>kg</Text>
+      </View>
+
+      <Pressable
+        onPressIn={checkMoreInfoInputs}
+        style={{ height: 50, width: 300, backgroundColor: "#F27A29", alignSelf: "center" }}>
+        <Text style={{ textAlign: "center", marginTop: 12.5 }}>CREATE ACCOUNT</Text>
+      </Pressable>
+
+    </View>
+
+
   );
 }
 
 const Stack = createStackNavigator();
 const register = () => {
-
-
-  
-
-  //Não apagar!
-  /*   function createUser() {
-  
-  
-      try {
-        fetch('http://192.168.1.66:3000/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "name": "setName",
-            "email": email,
-            "password": "test",
-            "height": "1.80",
-            "birth": "04-06-2000",
-            "weight": "70",
-            "photoUrl": "xskk"
-          })
-  
-        })
-  
-      } catch (error) {
-        console.log(error);
-      }
-    }
-   */
-
 
   return (
 
@@ -174,7 +233,7 @@ const register = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="RegisterScreen">
         <Stack.Screen options={{ headerShown: false }} name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Results" component={MoreRegisterInfo} />
+        <Stack.Screen options={{ headerShown: false }} name="MoreInfo" component={MoreInfo} />
       </Stack.Navigator>
     </NavigationContainer>
 
@@ -198,7 +257,6 @@ const styles = StyleSheet.create({
   },
   slide3: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#26282B'
   },
